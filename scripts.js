ff13adf88d168b444a714674f61b4dfd59605524
bazliamin90 <bez.aminuddin@gmail.com>
2024-08-03 22:11:16 +0800

    add

--- scripts.js
+++ scripts.js
@@ -37,4 +37,19 @@ document.querySelectorAll('.audio-player').forEach(player => {
         seconds = Math.floor(seconds % 60);
         return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
     }
-});
+	});
+
+function search_animal() {
+  let input = document.getElementById('searchbar').value
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName('animals');
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    }
+    else {
+      x[i].style.display = "list-item";
+    }
+  }
+}
